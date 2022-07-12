8c49b98617e17c71423c1fca02f016c9b4fed372
acheatham87 <acheatham87@gmail.com>
2022-07-12 13:43:57 -0500

    Finished backend for Tickets 35, 34, 33, and 32

---
+++ Tabloid/Repositories/TagRepository.cs
@@ -0,0 +1,131 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using Tabloid.Models;
+using Tabloid.Utils;
+
+namespace Tabloid.Repositories
+{
+    public class TagRepository : BaseRepository, ITagRepository
+    {
+        public TagRepository(IConfiguration configuration) : base(configuration) { }
+
+        public List<Tag> GetAllTags()
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        SELECT Id, Name 
+                        FROM Tag";
+                    
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        var tags = new List<Tag>();
+                        while(reader.Read())
+                        {
+                            tags.Add(new Tag()
+                            {
+                                Id = DbUtils.GetInt(reader, "Id"),
+                                Name = DbUtils.GetString(reader, "Name"),
+                            });
+                        }
+
+                        return tags;
+                    }
+                }
+            }
+        }
+
+        public Tag GetTagById(int id)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                    SELECT Id, Name
+                    FROM Tag
+                    WHERE Id = @id";
+
+                    DbUtils.AddParameter(cmd, "@Id", id);
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        Tag tag = null;
+                        if(reader.Read())
+                        {
+                            tag = new Tag()
+                            {
+                                Id = id,
+                                Name = DbUtils.GetString(reader, "Name"),
+                            };
+                        }
+
+                        return tag;
+                    }
+                }
+
+            }
+        }
+
+        public void Add(Tag tag)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                    INSERT INTO Tag
+                    OUTPUT INSERTED.ID
+                    VALUES (@Name)";
+
+                    DbUtils.AddParameter(cmd, "@Name", tag.Name);
+
+                    tag.Id = (int)cmd.ExecuteScalar();
+                }
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        DELETE FROM Tag
+                        WHERE Id = @Id";
+
+                    DbUtils.AddParameter(cmd, "@id", id);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void UpdateTag(Tag tag)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        UPDATE Tag
+                        SET Name = @Name
+                        WHERE Id = @Id";
+
+                    DbUtils.AddParameter(cmd, "@Name", tag.Name);
+                    DbUtils.AddParameter(cmd, "@Id", tag.Id);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
