f48463bd32b045acb1eaf866f4a3562c6d642823
LeeRJennings <leerjennings@outlook.com>
2022-07-12 14:50:29 -0500

    merge main before push

---
+++ Tabloid/Repositories/IPostRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Tabloid.Models;
+
+namespace Tabloid.Repositories
+{
+    public interface IPostRepository
+    {
+        List<Post> GetAll();
+        Post GetById(int id);
+        void Add(Post post);
+        void Delete(int id);
+        void Update(Post post);
+    }
+}
--- Tabloid/client/src/components/ApplicationViews.js
+++ Tabloid/client/src/components/ApplicationViews.js
@@ -3,6 +3,7 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./Login";
 import Register from "./Register";
 import Hello from "./Hello";
+import { PostList } from "./Posts/PostList";
 
 export default function ApplicationViews({ isLoggedIn }) {
   return (
@@ -16,6 +17,9 @@ export default function ApplicationViews({ isLoggedIn }) {
           <Route path="login" element={<Login />} />
           <Route path="register" element={<Register />} />
           <Route path="*" element={<p>Whoops, nothing here...</p>} />
+        </Route>
+        <Route path="posts">
+          <Route index element={<PostList/>} />
         </Route>
       </Routes>
     </main>
