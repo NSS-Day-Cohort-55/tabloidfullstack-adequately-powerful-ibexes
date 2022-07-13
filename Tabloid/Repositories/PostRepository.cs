4a09d6b9a8667907f27d8edfa9a49981577f679f
wes-mitchell <wmdrums@gmail.com>
2022-07-13 14:18:08 -0500

    test

--- Tabloid/Repositories/PostRepository.cs
+++ Tabloid/Repositories/PostRepository.cs
@@ -25,9 +25,9 @@ namespace Tabloid.Repositories
                             @IsApproved, @CategoryId, @UserProfileId )";
                         DbUtils.AddParameter(cmd, "@Title", post.Title);
                         DbUtils.AddParameter(cmd, "@Content", post.Content);
-                        DbUtils.AddParameter(cmd, "@ImageLocation", post.ImageLocation));
+                        DbUtils.AddParameter(cmd, "@ImageLocation", post.ImageLocation);
                         DbUtils.AddParameter(cmd, "@CreateDateTime", post.CreateDateTime);
-                        DbUtils.AddParameter(cmd, "@PublishDateTime", post.PublishDateTime));
+                        DbUtils.AddParameter(cmd, "@PublishDateTime", post.PublishDateTime);
                         DbUtils.AddParameter(cmd, "@IsApproved", post.IsApproved);
                         DbUtils.AddParameter(cmd, "@CategoryId", post.CategoryId);
                         DbUtils.AddParameter(cmd, "@UserProfileId", post.UserProfileId);
