c22732419c0daa38fb69899e995043e470708ffd
wes-mitchell <wmdrums@gmail.com>
2022-07-14 10:07:43 -0500

    adds create post button to both my posts and all posts view

--- Tabloid/client/src/components/Header.js
+++ Tabloid/client/src/components/Header.js
@@ -50,14 +50,6 @@ export default function Header({ isLoggedIn }) {
               </NavItem>
             }
           </Nav>
-          <Nav className="mr-auto" navbar>
-            { /* When isLoggedIn === true, we will render the Home link */}
-            {isLoggedIn &&
-              <NavItem>
-                <NavLink tag={RRNavLink} to="/posts/create">Add Post</NavLink>
-              </NavItem>
-            }
-          </Nav>
           <Nav navbar>
             {isLoggedIn &&
               <>
